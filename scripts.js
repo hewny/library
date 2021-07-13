9b91920b7a43cb2f6fd93e59dee3f0eb673353a9
Tony Yoo <tony.s.yoo@gmail.com>
2021-07-13 14:46:31 -0400

    initial setup

---
+++ scripts.js
@@ -0,0 +1,14 @@
+let myLibrary = [];
+
+function Book() {
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.info = function () {
+		return `${title} by ${author}, ${pages} pages, ${pages}`
+	}
+}
+
+function addBookToLibrary() {
+	
+}
