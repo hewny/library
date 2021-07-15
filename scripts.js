f73c9531bba3ca6b186b0f161eefd1ebdeaf2c2b
Tony Yoo <tony.s.yoo@gmail.com>
2021-07-15 11:07:12 -0400

    added logic for adding a new book and updating the displayed books

--- scripts.js
+++ scripts.js
@@ -11,20 +11,34 @@ function Book() {
 
 function addBookToLibrary(e) {
 	myLibrary.push(e)
+	clearInput()
+	hideModal()
+	updateBooks()
 }
 
 
 function updateBooks() {
 	// clear previous entries
-	container.querySelectorAll('div').forEach(div => div.remove())
+	cardContainer.querySelectorAll('.card').forEach(div => div.remove())
 	
 	// reload books
 	for (let i=0; i < myLibrary.length; i++) {
 		newDiv = document.createElement("div");
-		newDiv.className = "book";
-		newDiv.textContent = `${myLibrary[i].title}`;
-		container.appendChild(newDiv)
-		console.log(myLibrary)
+		newDiv.className = "card";
+		newImg = document.createElement('img');
+		newImg.src="book.png"
+		newH1 = document.createElement('h1');
+		newH1.textContent = myLibrary[i]["title"];
+		newH2 = document.createElement('h2');
+		newH2.textContent = myLibrary[i]["author"];
+		newH3 = document.createElement('h3');
+		newH3.textContent = myLibrary[i]["pages"]+" pages";
+		
+		newDiv.appendChild(newImg);
+		newDiv.appendChild(newH1);
+		newDiv.appendChild(newH2);
+		newDiv.appendChild(newH3);
+		cardContainer.appendChild(newDiv);
 	}
 }
 
@@ -63,9 +77,10 @@ function updateBooks() {
 // }
 
 function getValues() {
-	newBook = new Book()
-	newBook.title = document.querySelector('#inputTitle').value
-	console.log(newBook.title)
+	let newBook = new Book()
+	newBook.title = inputTitle.value
+	newBook.author = inputAuthor.value
+	newBook.pages = inputPages.value
 	addBookToLibrary(newBook)
 }
 
@@ -73,15 +88,37 @@ function logconsole(e) {
 	console.log(e)
 }
 
+function evalRead() {
+	if (readNo.checked) {
+		return false
+	}
+	else return true
+}
+
+function clearInput() {
+	inputTitle.value = ""
+	inputAuthor.value = ""
+	inputPages.value = ""
+	readNo.checked = false
+	readYes.checked = false
+}
+
 const container = document.querySelector(".container");
 const modalOverlay = document.querySelector('.modal-overlay');
 const modalContainer = document.querySelector('.modal-container');
 const modalContent = document.querySelector('.modal-content');
+const cardContainer = document.querySelector('.card-container')
 
 const addButton = document.querySelector('.add-button');
 const addExampleButton = document.querySelector('.add-example-button');
 const addBookButton = document.querySelector('.add-book-button');
 const closeButton = document.querySelector('.close-button');
+
+const inputTitle = document.querySelector('#inputTitle');
+const inputAuthor = document.querySelector('#inputAuthor');
+const inputPages = document.querySelector('#inputPages');
+const readNo = document.querySelector('#readNo');
+const readYes = document.querySelector('#readYes');
 
 function showModal() {
 	modalOverlay.classList.remove('closed')
@@ -95,3 +132,4 @@ function hideModal() {
 
 addButton.addEventListener('click', showModal)
 closeButton.addEventListener('click',hideModal)
+addBookButton.addEventListener('click',getValues)
